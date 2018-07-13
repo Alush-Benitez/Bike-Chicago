352824f1b5edfbad4f1064435aeaab204d68c121
Michael Filippini <michaelfilippini@michaels-macbook-pro-2.local>
2018-07-13 13:30:48 -0500

    Map and new ViewControllers

---
+++ Bike Chicago/ContainerViewController.swift
@@ -0,0 +1,36 @@
+//
+//  ContainerViewController.swift
+//  Bike Chicago
+//
+//  Created by Michael Filippini on 7/13/18.
+//  Copyright © 2018 Alush Benitez. All rights reserved.
+//
+
+import UIKit
+import QuartzCore
+
+class ContainerViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
