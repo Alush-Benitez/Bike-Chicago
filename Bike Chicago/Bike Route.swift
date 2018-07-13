fb433af0364a33ef23833d2c95825dae2364c967
Alush Benitez <alushb5@yahoo.com>
2018-07-13 14:00:58 -0500

    work on custom class

--- Bike Chicago/Bike Route.swift
+++ Bike Chicago/Bike Route.swift
@@ -9,4 +9,23 @@
 import Foundation
 import MapKit
 
-//comment
+class BikeRoute: MKPolyline {
+    var routeType = ""
+    var streetName = ""
+    var startStreet = ""
+    var endStreet = ""
+    var lengthInFeet = 0.0
+    var coordinates: [Double] = []
+    
+    init(routeType: String, streetName: String, startStreet: String, endStreet: String, lengthInFeet: Double){
+        self.routeType = routeType
+        self.streetName = streetName
+        self.startStreet = startStreet
+        self.endStreet = endStreet
+        self.lengthInFeet = lengthInFeet
+    }
+    
+    init?(json: [Any]) {
+        self.routeType = json[bikeroute] as? String
+    }
+}
