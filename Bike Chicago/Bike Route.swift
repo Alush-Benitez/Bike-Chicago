3a92361a1509576117fdd699256d00c41ef8ae90
Michael Filippini <michaelfilippini@michaels-macbook-pro-2.local>
2018-07-16 14:56:16 -0500

    Updated load times and reworked query

--- Bike Chicago/Bike Route.swift
+++ Bike Chicago/Bike Route.swift
@@ -10,89 +10,27 @@ import Foundation
 import MapKit
 
 
-
-var points = [CLLocationCoordinate2D]()
-
 class BikeRoute: MKPolyline {
-    //    var routeType = ""
-    //    var streetName = ""
-    //    var startStreet = ""
-    //    var endStreet = ""
-    //    var lengthInFeet = 0.0
     
-    var completeBikeData = [Dictionary<String, Any>]()
-    var separatePathCoordinates = [[CLLocationCoordinate2D]]()
-    var compiledPathCoordinates = [CLLocationCoordinate2D]()
-    
-    var polylines: [MKPolyline] = []
-    
-    var count = 0
+    var routeType = ""
+    var streetName = ""
+    var startStreet = ""
+    var endStreet = ""
+    var lengthInFeet = 0.0
+    var routeLine = MKPolyline()
     
     
-    let query = "https://data.cityofchicago.org/resource/hvv9-38ut.json"
-    //    var cdn = [Array<Any>]()
-    
-    var lengths = [Double]()
-    var routeTypes = [String]()
-    var streetNames = [String]()
-    var startStreets = [String]()
-    var endStreets = [String]()
-    
-    override init(/*routeType: String, streetName: String, startStreet: String, endStreet: String, lengthInFeet: Double*/){
+    init(routeType: String, streetName: String, startStreet: String, endStreet: String, lengthInFeet: Double, route: MKPolyline){
         super.init()
-        getJSONFromURL()
-        //print(self.separatePathCoordinates)
+        self.routeType = routeType
+        self.streetName = streetName
+        self.startStreet = startStreet
+        self.endStreet = endStreet
+        self.lengthInFeet = lengthInFeet
+        self.routeLine = route
     }
     
-    func parse(json: JSON) {
-        for result in json.arrayValue {
-            //if count < 1{
-                self.routeTypes.append(result["bikeroute"].stringValue)
-                self.streetNames.append(result["street"].stringValue)
-                self.startStreets.append(result["f_street"].stringValue)
-                self.endStreets.append(result["t_street"].stringValue)
-                self.lengths.append(result["shape_leng"].doubleValue)
-                
-                var b = 0
-                for _ in result["the_geom"]["coordinates"] {
-                    let lat = result["the_geom"]["coordinates"][b][0].stringValue
-                    let long = result["the_geom"]["coordinates"][b][1].stringValue
-                    let oneCoordinate = CLLocationCoordinate2DMake(Double(long)!, Double(lat)!)
-                    self.compiledPathCoordinates.append(oneCoordinate)
-                    b += 1
-                    if b == (result["the_geom"]["coordinates"].arrayValue).count {
-                        let myPolyline = MKPolyline.init(coordinates: compiledPathCoordinates, count: compiledPathCoordinates.count)
-                        polylines.append(myPolyline)
-                        compiledPathCoordinates.removeAll()
-                    }
-                //}
-                count += 1
-            }
-            
-        }
-    }
     
-    func getJSONFromURL() {
-        if let url = URL(string: query) {
-            if let data = try? Data(contentsOf: url) {
-                let json = try! JSON(data: data)
-                self.parse(json: json)
-            }
-        }
-    }
-    
-    func mapView(_ mapView: MKMapView, rendererFor overlay: MKOverlay) -> MKOverlayRenderer {
-        print("blahj")
-        if overlay is MKPolyline {
-            
-            let polyLineRenderer = MKPolylineRenderer(overlay: overlay)
-            polyLineRenderer.strokeColor = .blue
-            polyLineRenderer.lineWidth = 2.0
-            
-            return polyLineRenderer
-        }
-        return MKPolylineRenderer()
-    }
 }
 
 
--- Bike Chicago/ViewController.swift
+++ Bike Chicago/ViewController.swift
@@ -14,10 +14,8 @@ class ViewController: UIViewController, MKMapViewDelegate {
     
     @IBOutlet var mapView: MKMapView!
     
-    
-    
     let regionRadius: CLLocationDistance = 10000
-    
+    var bikeRoutes = [BikeRoute]()
     var count = 0
     
     
@@ -26,23 +24,11 @@ class ViewController: UIViewController, MKMapViewDelegate {
     override func viewDidLoad() {
         super.viewDidLoad()
         mapView.delegate = self
-        
-        points.append(CLLocationCoordinate2DMake(35.3289, -120.7394))
-        points.append(CLLocationCoordinate2DMake(35.3287, -120.7396))
-        points.append(CLLocationCoordinate2DMake(35.3284, -120.7392))
-        points.append(CLLocationCoordinate2DMake(35.3286, -120.7389))
-
-        let polyline = MKPolyline(coordinates: points, count: points.count)
-        mapView.add(polyline)
-        
-        
-        addRoute()
-        
         let initialLocation = CLLocation(latitude: 41.8781, longitude: -87.6298)
         centerMapOnLocation(location: initialLocation)
         mapView.showsUserLocation = true
         
-        
+        grabData()
     }
     
     override func didReceiveMemoryWarning() {
@@ -50,24 +36,90 @@ class ViewController: UIViewController, MKMapViewDelegate {
         // Dispose of any resources that can be recreated.
     }
     
+    func grabData() {
+        let query = "https://data.cityofchicago.org/resource/hvv9-38ut.json"
+        if let url = URL(string: query) {
+            if let data = try? Data(contentsOf: url) {
+                let json = try! JSON(data: data)
+                self.parse(json: json)
+            }
+        }
+    }
+    
+    let group = DispatchGroup()
+    
+    
+    func parse(json: JSON?) {
+        for i in 0..<120 {
+            addResults(result: (json?.arrayValue[i])!)
+        }
+        
+        for i in 120..<240 {
+            addResults(result: (json?.arrayValue[i])!)
+        }
+        
+        for i in 240..<360 {
+            addResults(result: (json?.arrayValue[i])!)
+        }
+        
+        for i in 360..<480 {
+            addResults(result: (json?.arrayValue[i])!)
+        }
+        
+        for i in 480..<(json?.arrayValue.count)! {
+            addResults(result: (json?.arrayValue[i])!)
+        }
+        addRoutes()
+    }
+    
+    
+    func addResults(result: JSON){
+        
+        var routeType = ""
+        var streetName = ""
+        var startStreet = ""
+        var endStreet = ""
+        var coords = [CLLocationCoordinate2D]()
+        var routeLength = 0.0
+        
+        routeType = (result["bikeroute"].stringValue)
+        streetName = (result["street"].stringValue)
+        startStreet = (result["f_street"].stringValue)
+        endStreet = (result["t_street"].stringValue)
+        routeLength = (result["shape_leng"].doubleValue)
+        
+        let coordPairs = ((result["the_geom"])["coordinates"].arrayValue)
+        
+        for coordPair in coordPairs {
+            print(" INDEX 1\(Double(coordPair[1].stringValue)!)")
+            print(" INDEX 0\(Double(coordPair[0].stringValue)!)")
+            let coord = CLLocationCoordinate2DMake(Double(coordPair[1].stringValue)!, Double(coordPair[0].stringValue)!)
+            coords.append(coord)
+            
+        }
+        
+        let route = MKPolyline(coordinates: coords, count: coords.count)
+        
+        bikeRoutes.append(BikeRoute(routeType: routeType, streetName: streetName, startStreet: startStreet, endStreet: endStreet, lengthInFeet: routeLength, route: route))
+    }
+    
+    
+    
     func centerMapOnLocation(location: CLLocation) {
         let coordinateRegion = MKCoordinateRegionMakeWithDistance(location.coordinate,
                                                                   regionRadius, regionRadius)
         mapView.setRegion(coordinateRegion, animated: true)
     }
     
-    func addRoute() {
-        print(BikeRoute().polylines.count)
-        for polyline in BikeRoute().polylines {
-            mapView.add(polyline)
-            print("Polyline: \(polyline.debugDescription)\n")
+    func addRoutes() {
+        for route in bikeRoutes {
+            print(route.routeLine.debugDescription)
+            mapView.add(route.routeLine)
         }
     }
-
+    
     func mapView(_ mapView: MKMapView, rendererFor overlay: MKOverlay) -> MKOverlayRenderer {
-        print("blahj")
         if overlay is MKPolyline {
-            print("Inside yay")
             let polyLineRenderer = MKPolylineRenderer(overlay: overlay)
             polyLineRenderer.strokeColor = .blue
             polyLineRenderer.lineWidth = 2.0
